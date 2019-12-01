921cfaa7c81acef77c62fa23beb2ef0916935978
Ahesanul Mahbub Shovon <33129132+ams-cse@users.noreply.github.com>
2019-12-01 23:36:49 +0600

    nodeJS

---
+++ basic-nodeJS/first.js
@@ -0,0 +1,39 @@
+// var name = "Shovon";
+// var age = 20;
+// var hasHobby = true;
+// function print(userName, userAge, userHobby) {
+//    return('Name is '+ userName + ', His age is '+ userAge + ' and Hobby is '+ userHobby);
+// }
+// console.log(print(name, age, hasHobby));
+//
+//
+// const people = {
+//         nameOne: 'Mahbub',
+//         ageOne: 22,
+//         feel(){
+//             console.log('I am '+ this.nameOne);
+//     }
+// };
+// people.feel();
+
+
+// fName = ['Siddik', ' Islamul', ' Arju'];
+// fName.push(' Forhad');
+// console.log("My Friends Name is : "+fName);
+//
+// setTimeout(() => {
+//     console.log("Hello Node JS");
+// }, 1000);
+// console.log("Hi!");
+//
+
+
+// var http = require('http');
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.write('Hello, I am Shovon. This is Node Js Server!');
+//     res.end('Thank you... NodeJS.');
+// }).listen(8080);
+
+
